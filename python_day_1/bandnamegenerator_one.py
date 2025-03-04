26caf639b76a8363829425031e3963b682227abd
YuriIgor <146781246+YuriIgor@users.noreply.github.com>
2025-03-04 15:33:59 -0300

    band_name_generators

---
+++ python_day_1/bandnamegenerator_one.py
@@ -0,0 +1,9 @@
+import time
+
+print("Welcome to the Band Name Generator\n")
+time.sleep(2)
+city = input("What's the name of the city you grew up in?\n")
+adjective = input("First adjective that comes to your mind:\n")
+pet = input("What's the name or the species of your pet?\n")
+time.sleep(2)
+print(f"You band name could be {adjective} {pet} of {city}.")
---
+++ python_day_1/bandnamegenerator_two.py
@@ -0,0 +1,10 @@
+import time
+
+print("Este é o gerador de nomes para bandas.\n")
+time.sleep(2)
+noun = input("Qual o primeiro substantivo que te vem à cabeça?\n")
+adjective = input("Qual é o primeiro adjetivo que te vem à cabeça?\n")
+time.sleep(2)
+print(f"O nome da sua banda pode ser {noun} {adjective} ou {adjective} {noun}.\n")
+time.sleep(1)
+print("Até a próxima!")
